8a652f0ec5b6980ce7d37e9c0274890163ec6683
Antonioha <antonio9hanania@gmail.com>
2024-05-08 11:32:29 +0300

    Update responsive-text.js

---
+++ responsive-text.js
@@ -0,0 +1,64 @@
+<style>
+    body {
+      direction: rtl;
+      text-align: center;
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+
+    main {
+      max-width: 1200px;
+      margin: 0 auto;
+      padding: 20px;
+    }
+
+    h1 {
+      font-size: 24px;
+      margin-bottom: 20px;
+    }
+
+    .iframe-container {
+      position: relative;
+      width: 100%;
+      margin-bottom: 20px;
+    }
+
+    .iframe-container iframe {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+
+    .aspect-ratio-text {
+      padding-bottom: 18%; /* Adjust the percentage based on the height of the text */
+    }
+
+
+	.aspect-ratio-10-3 {
+      padding-bottom: 30%; /* 16:9 aspect ratio */
+    }
+	
+	.aspect-ratio-100-41 {
+      padding-bottom: 41%; /* 100:41 aspect ratio */
+    }
+	
+	.aspect-ratio-16-9 {
+      padding-bottom: 56.25%; /* 16:9 aspect ratio */
+    }    
+
+    .aspect-ratio-4-3 {
+      padding-bottom: 75%; /* 4:3 aspect ratio */
+    }  
+
+    @media screen and (max-width: 768px) {
+      h1 {
+        font-size: 20px !important;
+      }
+    }
+	div span{
+		20px !important;
+	}
+  </style>
